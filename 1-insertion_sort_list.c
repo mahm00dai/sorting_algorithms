248370581a13dc5b5ac02fd593f1847fefc15436
mahm00dai <mahmoud.ai.abdelsalam@gmail.com>
2024-10-29 01:19:34 -0700

    Sorting

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * swap_nodes - Swaps two nodes in a doubly linked list.
+ *
+ * @a: First node to swap.
+ * @b: Second node to swap.
+ */
+void swap_nodes(listint_t *a, listint_t *b)
+{
+	listint_t *temp;
+
+	if (a == NULL || b == NULL || a == b)
+		return;
+
+	temp = a->prev;
+	if (temp)
+		temp->next = b;
+	b->prev = temp;
+
+	a->next = b->next;
+	if (b->next)
+		b->next->prev = a;
+
+	b->next = a;
+	b->prev = temp;
+	a->prev = b;
+
+	if (!b->prev)
+		*b = *a; // Update the head if necessary
+
+	print_list(a);
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list using the Insertion sort algorithm.
+ *
+ * @list: Pointer to the head of the doubly linked list.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *insert_pos;
+
+	if (!list || !*list)
+		return;
+
+	current = (*list)->next;
+	while (current)
+	{
+		insert_pos = current->prev;
+		while (insert_pos && current->n < insert_pos->n)
+		{
+			swap_nodes(insert_pos, current);
+			if (!current->prev) // If current is now the new head
+				*list = current;
+			insert_pos = current->prev;
+		}
+		current = current->next;
+	}
+}
+
--- sort.h
+++ sort.h
@@ -3,8 +3,17 @@
 
 #include <stddef.h>
 
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void print_list(const listint_t *list);
 
 #endif /* SORT_H */
 
