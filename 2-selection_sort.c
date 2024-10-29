cc02dccc2214a0307a598c5b6a267a3c4b2a977f
mahm00dai <mahmoud.ai.abdelsalam@gmail.com>
2024-10-29 01:28:01 -0700

    Sorting

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "sort.h"
+
+void print_array(int *array, size_t size);
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j, min_idx;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+		if (min_idx != i)
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
+		}
+	}
+}
+
