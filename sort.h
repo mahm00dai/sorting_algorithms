d6b00a18a50fcf0393935e2245f867b77f066b39
mahm00dai <mahmoud.ai.abdelsalam@gmail.com>
2024-11-03 02:33:40 -0800

    Sorting

--- sort.h
+++ sort.h
@@ -30,7 +30,7 @@ void quick_sort_recursive(int *array, int low, int high);
 void shell_sort(int *array, size_t size);
 void swap_nodes(listint_t *node_a, listint_t *node_b, listint_t **list);
 void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
+/*void counting_sort(int *array, size_t size);*/
 void merge(int *array, size_t left, size_t mid, size_t right);
 void merge_sort_recursive(int *array, size_t left, size_t right);
 void merge_sort(int *array, size_t size);
